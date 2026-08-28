78570855f46d7711fc7870b008295ace79f2c557
Clara Elisa <claraelisa.davila@gmail.com>
2026-08-28 08:59:14 -0300

    commit

--- pag.mapa/pMapa.js
+++ pag.mapa/pMapa.js
@@ -32,3 +32,19 @@ anterior.onclick = () => {
 }
 
 atualizarGaleria()
+
+function mensagem() {
+    alert("🌱 Obrigado por fazer parte do EcoViva!");
+}
+
+function copiarEmail() {
+    const email = document.getElementById("email").textContent;
+
+    navigator.clipboard.writeText(email)
+        .then(() => {
+            alert("📋 E-mail copiado!");
+        })
+        .catch(() => {
+            alert("❌ Não foi possível copiar o e-mail.");
+        });
+}
