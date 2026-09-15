ff26a756617fb7273aad84704a824fc80187aa5a
LuciFlores208 <luci.barbosaflores@gmail.com>
2026-09-15 11:33:49 -0300

    commit

--- pag.calculadora/script.js
+++ pag.calculadora/script.js
@@ -28,3 +28,19 @@ document.getElementById('form').addEventListener('submit', e => {
     r.style.display = 'block';
     r.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
 });
+
+function mensagem() {
+    alert("🌱 Obrigado por fazer parte do EcoViva!");
+}
+
+function copiarEmail() {
+    const email = document.getElementById("email").textContent;
+
+    navigator.clipboard.writeText(email)
+        .then(() => {
+            alert("📋 E-mail copiado!");
+        })
+        .catch(() => {
+            alert("❌ Não foi possível copiar o e-mail.");
+        });
+}
